b27911031ebd14e3dd94a511a0002ea34c8d1e09
saarsc <scsaar@gmail.com>
2018-07-15 17:56:19 +0300

    Contact activity semi-done might need a redesign

---
+++ app/src/main/java/israeltravelinsurance/israeltravelinsurance/ExpandableListAdapter.java
@@ -0,0 +1,142 @@
+package israeltravelinsurance.israeltravelinsurance;
+
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Typeface;
+import android.net.Uri;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.List;
+
+
+
+/**
+ * Created by reale on 21/11/2016.
+ */
+
+public class ExpandableListAdapter extends BaseExpandableListAdapter {
+    private Activity context;
+    private List<String> listDataHeader;
+    private HashMap<String,List<contactMenu.childItem>> listHashMap;
+    private int myPremmision;
+    public ExpandableListAdapter(Activity context, List<String> listDataHeader, HashMap<String, List<contactMenu.childItem>> listHashMap) {
+        this.context = context;
+        this.listDataHeader = listDataHeader;
+        this.listHashMap = listHashMap;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return listDataHeader.size();
+    }
+
+    @Override
+    public int getChildrenCount(int i) {
+        return listHashMap.get(listDataHeader.get(i)).size();
+    }
+
+    @Override
+    public Object getGroup(int i) {
+        return listDataHeader.get(i);
+    }
+
+    @Override
+    public Object getChild(int i, int i1) {
+        return listHashMap.get(listDataHeader.get(i)).get(i1); // i = Group Item , i1 = ChildItem
+    }
+
+    @Override
+    public long getGroupId(int i) {
+        return i;
+    }
+
+    @Override
+    public long getChildId(int i, int i1) {
+        return i1;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int i, boolean b, View view, ViewGroup viewGroup) {
+        String headerTitle = (String)getGroup(i);
+        if(view == null)
+        {
+            LayoutInflater inflater = (LayoutInflater)this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.group_item,null);
+        }
+        TextView lblListHeader = view.findViewById(R.id.lblListHeader);
+        lblListHeader.setTypeface(null, Typeface.BOLD);
+        lblListHeader.setText(headerTitle);
+        return view;
+    }
+
+    @Override
+    public View getChildView(int i, int i1, boolean b, View view, ViewGroup viewGroup) {
+        final String childTitle = ((contactMenu.childItem) getChild(i,i1)).getTitle();
+        final String childHint = ((contactMenu.childItem) getChild(i,i1)).getHint();
+
+        if(view == null)
+        {
+            LayoutInflater inflater = (LayoutInflater)this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.list_item,null);
+        }
+
+        TextView txtListChild = view.findViewById(R.id.lblListItem);
+        TextView hintListChild = view.findViewById(R.id.lblListHint);
+        txtListChild.setText(childTitle);
+        txtListChild.setOnClickListener(view1 -> {
+            if(childTitle.contains("טלפון")){
+                Uri callUri = Uri.parse("tel://"+childHint);
+                Intent callIntent = new Intent(Intent.ACTION_CALL, callUri);
+                callIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_NO_USER_ACTION);
+                if (ActivityCompat.checkSelfPermission(context, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(context,
+                            new String[]{Manifest.permission.CALL_PHONE},
+                            myPremmision);
+                }else {
+                    context.startActivity(callIntent);
+                }
+            }else if(childTitle.contains("דו\"אל")){
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("message/rfc822");
+                intent.putExtra(Intent.EXTRA_EMAIL, new String[]{childHint});
+            try {
+                context.startActivity(Intent.createChooser(intent, "שלח מייל..."));
+            } catch (android.content.ActivityNotFoundException ex) {
+                Snackbar.make(context.findViewById(R.id.fab), "אנא התקן האפליקצייה התומכת בשליחת מיילים", Snackbar.LENGTH_SHORT).show();
+            }
+            }else if(childHint.equals("")){
+                try {
+                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://israeltravelinsurance.co.il/PDF/PassportCard%20contact.pdf"));
+                    context.startActivity(browserIntent);
+                } catch (ActivityNotFoundException e) {
+                    Snackbar.make(context.findViewById(R.id.fab), "אנא התקן האפליקצייה התומכת בפתיחת קישורים", Snackbar.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                }
+            }
+        });
+        hintListChild.setText(childHint);
+        return view;
+    }
+
+    @Override
+    public boolean isChildSelectable(int i, int i1) {
+        return true;
+    }
+}
--- app/src/main/java/israeltravelinsurance/israeltravelinsurance/contactMenu.java
+++ app/src/main/java/israeltravelinsurance/israeltravelinsurance/contactMenu.java
@@ -1,13 +1,92 @@
 package israeltravelinsurance.israeltravelinsurance;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ExpandableListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class contactMenu extends AppCompatActivity {
-
+    private ExpandableListView listView;
+    private ExpandableListAdapter listAdapter;
+    private List<String> listDataHeader;
+    private HashMap<String,List<childItem>> listHash;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_menu);
+
+        listView = findViewById(R.id.lvExp);
+        initData();
+        listAdapter = new ExpandableListAdapter(this,listDataHeader,listHash);
+        listView.setAdapter(listAdapter);
+    }
+
+    private void initData() {
+        listDataHeader = new ArrayList<>();
+        listHash = new HashMap<>();
+
+        listDataHeader.add("סוכנות");
+        listDataHeader.add("מגדל");
+        listDataHeader.add("הפניקס");
+        listDataHeader.add("הראל");
+        listDataHeader.add("PassportCard");
+        listDataHeader.add("מנורה");
+
+        List<childItem> mainCompany = new ArrayList<>();
+        mainCompany.add(new childItem("טלפון","072-2222-123"));
+        mainCompany.add(new childItem("דו\"אל","info@israeltravelinsurance.co.il"));
+
+        List<childItem> migdal = new ArrayList<>();
+        migdal.add(new childItem("טלפון בין לאומי","972-3-9206912"));
+        migdal.add(new childItem("טלפון ארה\"ב","717-3-128917"));
+        migdal.add(new childItem("טלפון מהארץ","03-9206912"));
+        migdal.add(new childItem("פקס","972-3-9226380"));
+        migdal.add(new childItem("דו\"אל","migdal@ima-mc.com"));
+
+        List<childItem> afniex = new ArrayList<>();
+        afniex.add(new childItem("טלפון","03-9206912"));
+        afniex.add(new childItem("דו\"אל","fnx@ima-mc.com"));
+
+        List<childItem> arhel = new ArrayList<>();
+        arhel.add(new childItem("טלפון","972-3-7547030"));
+        List<childItem> passportCard = new ArrayList<>();
+        passportCard.add(new childItem("לכל המספרים מסביב לעולם",""));
+
+        List<childItem> menora = new ArrayList<>();
+        menora.add(new childItem("טלפון","972-3-9206911"));
+        menora.add(new childItem("מוקד איתור וחילוץ \"מגנוס\" בטלפון","972-03-6006505"));
+
+        listHash.put(listDataHeader.get(0),mainCompany);
+        listHash.put(listDataHeader.get(1),migdal);
+        listHash.put(listDataHeader.get(2),afniex);
+        listHash.put(listDataHeader.get(3),arhel);
+        listHash.put(listDataHeader.get(4),passportCard);
+        listHash.put(listDataHeader.get(5),menora);
+    }
+    public class childItem{
+        private String title;
+        private String hint;
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String getHint() {
+            return hint;
+        }
+
+        public childItem(String title, String hint) {
+            this.title = title;
+            this.hint = hint;
+        }
+
+    }
+    @Override
+    public void onBackPressed(){
+        startActivity(new Intent(this,MainActivity.class));
     }
 }
